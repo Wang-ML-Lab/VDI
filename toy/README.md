6f621bde9ca1c41c1548274a524a49f1ade501f4
shsjxzh <1927045093@qq.com>
2023-03-10 01:04:19 -0500

    add something

--- toy/README.md
+++ toy/README.md
@@ -3,6 +3,8 @@ If you have any questions, feel free to pose an issue or send an email to zihao.
 
 The code for VDI is developed based on [CIDA](https://github.com/hehaodele/CIDA). [CIDA](https://github.com/hehaodele/CIDA) also provides many baseline implementations (e.g., DANN, MDD), which we used for performance comparasion.
 
+Notice that we have pretrained the network of q(u|x) and p(x|u) and we included a pretrained weight in this code base. If you want to rerun the pretraining, you need to use the DANN discriminator (set  "opt.d_loss_type = "DANN_loss"" ) and set \sigma_p(x|u) very small (that is, set "opt.lambda_reconstruct" to be about 200~500).
+
 ## DG-15
 ### How to Train on DG-15
     python main.py -c config_DG_15 (or)
--- toy/configs/config_DG_60.py
+++ toy/configs/config_DG_60.py
@@ -3,33 +3,33 @@ import numpy as np
 # set experiment configs
 opt = EasyDict()
 
-opt.src_domain_idx = [0, 12, 3, 4, 14, 8]
-opt.tgt_domain_idx = [1, 2, 5, 6, 7, 9, 10, 11, 13]
-opt.all_domain_idx = opt.src_domain_idx + opt.tgt_domain_idx
-opt.num_source = len(opt.src_domain_idx)
-opt.num_target = len(opt.tgt_domain_idx)
-opt.num_domain = len(opt.all_domain_idx)
+# now it is quarter
+opt.num_domain = 60
+opt.num_source = 6
+opt.num_target = opt.num_domain - opt.num_source
+opt.src_domain_idx = np.array([40,32,12,29,22,52])  # tight_boundary
+opt.tgt_domain_idx = np.array(list(set(range(0, opt.num_domain)) - set(opt.src_domain_idx)))
 
-opt.dataset = "data/toy_d15_spiral_tight_boundary.pkl"
+opt.dataset = "data/toy_d60_pi_random.pkl"
 opt.d_loss_type = "GRDA_loss" # "DANN_loss" # "CIDA_loss" # "DANN_loss_mean"
 
 opt.use_pretrain_R = True
-opt.pretrain_R_path =  "data/netR_4_dann.pth" 
-opt.pretrain_U_path = "data/netU_4_dann.pth" 
+opt.pretrain_R_path = "data/netR_4_dann_60_pi.pth" # "data/netR_4_dann.pth"
+opt.pretrain_U_path = "data/netU_4_dann_60_pi.pth" # "data/netU_4_dann.pth"
 opt.fix_u_r = False
 
-opt.lambda_gan = 0.6
-opt.lambda_reconstruct = 10
-opt.lambda_u_concentrate = 1
+opt.lambda_gan = 0.35
+opt.lambda_reconstruct = 7 
+opt.lambda_u_concentrate = 0.4
 opt.lambda_beta = 0.8
 opt.lambda_beta_alpha = 0.1
 
 # for warm up
-opt.init_lr = 1e-6
-opt.peak_lr_e = 3.2*1e-4  
-opt.peak_lr_d = 3.2*1e-4  
+opt.init_lr = 3e-7
+opt.peak_lr_e = 1e-4
+opt.peak_lr_d = 1e-4
 opt.final_lr = 1e-8
-opt.warmup_steps = 70
+opt.warmup_steps = 40
 
 opt.seed = 2333 
 opt.num_epoch = 500
@@ -40,8 +40,8 @@ opt.visdom_port = 2000
 opt.test_on_all_dmn = False
 opt.outf = "dump"
 
-opt.save_interval = 100
-opt.test_interval = 20  # 20
+opt.save_interval = 50
+opt.test_interval = 10  # 20
 
 opt.device = "cuda"
 opt.gpu_device = "0"
@@ -60,4 +60,4 @@ opt.u_dim = 4      # the dimension of local domain index u
 opt.beta_dim = 2   # the dimension of global domain index beta
 
 # for grda discriminator
-opt.sample_v = 10
+opt.sample_v = 30
